3c915e3186a88172373777a7d3d1af0051b079a9
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-30 22:04:33 -0300

    go

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -19,7 +19,6 @@ resource "aws_route_table" "rt_pub" {
   vpc_id = aws_vpc.vpc_pub.id
   route {
     cidr_block                = var.network_vpc_priv_cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
   }
   route {
     cidr_block = "0.0.0.0/0"
@@ -28,10 +27,8 @@ resource "aws_route_table" "rt_pub" {
 }
 
 resource "aws_route_table" "rt_priv" {
-  vpc_id = aws_vpc.vpc_priv.id
   route {
     cidr_block                = var.network_vpc_pub_cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
   }
 }
 
