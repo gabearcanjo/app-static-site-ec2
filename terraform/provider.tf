9d660f20cc36c727efd3a8d2abbdddd8374123f5
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-30 23:06:50 -0300

    vamo

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,16 +1,13 @@
+# PROVIDER
 terraform {
 
-  required_version = "~> 1.4"
+  required_version = "~> 1.5.4"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.13"
     }
   }
 
-}
-
-provider "aws" {
-  region                   = "us-east-1"
 }
